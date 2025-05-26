75f67b5dbd609dbd55a70d25f22f3d05e1589436
Siena <sienasaifi@gmail.com>
2025-05-26 14:26:49 +0200

    Se añadió Ejercicio2

---
+++ Ejercicio2.java
@@ -0,0 +1,10 @@
+package ejercicios;
+
+public class Ejercicio2 {
+	public static void main(String[] args) {
+		float x=18, y=19; 
+		System.out.println("MI EDAD:"+x); 
+		System.out.println("MI EDAD EL PRÓXIMO AÑO:"+y);
+	}
+	
+}
