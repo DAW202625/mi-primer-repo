4eaad73480817fbc86bc3da7980844cc4dd8ff5e
DAW202625 <daw202625@estudiantes.salesianasnsp.es>
2025-05-26 14:10:54 +0200

    Update Ejercicio1.java

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -1,5 +1,5 @@
 package ejercicios;
-
+// Editando programa para hacer commit
 public class Ejercicio1 {
 	public static void main(String[] args) {
 		System.out.println("Me guta la programación"); 
